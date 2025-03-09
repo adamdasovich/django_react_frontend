62b56e5d24b7176743da22e20d51e15fca56be93
Adam Dasovich <adamdasovich@gmail.com>
2025-03-09 09:28:33 -0400

    small change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,38 +1,87 @@
-import React, { useState, useEffect } from 'react';
-import AllTeamsTable from './tables/AllTeamsTable';
+import React, { useState, useEffect, useMemo } from 'react';
+// import AllTeamsTable from './tables/AllTeamsTable';
 import AxiosInstance from './axios';
-import { Box } from '@mui/material';
+import { Box, Typography, Chip } from '@mui/material';
+import ViewCompactIcon from '@mui/icons-material/ViewCompact';
+import {MaterialReactTable} from 'material-react-table'
 
 const Home = () => {
-  const [tableData, setTableData] = useState([]); // Initialize as an EMPTY ARRAY
+  const [myData, setMyData] = useState([]); // Initialize as an EMPTY ARRAY
 
   const getData = () => {
     AxiosInstance.get('club/')
-      .then(res => setTableData(res.data));
-
-    // AxiosInstance.get('club/1')
-    //   .then(res => console.log(res.data))
+      .then(res => {
+        setMyData(res.data);
+        console.log(res.data)
+      }) 
   };
 
   useEffect(() => {
     getData();
   }, []);
 
-  const deleteTeam = (id) => {
-    setTableData(prevData => prevData.filter(team => team.id !== id))
-    AxiosInstance.delete(`club/${id}/`)
-      .then(() => console.log('Team Deleted'))
-      .catch(error => console.error('Error deleting team: ', error))
-  }
+  const columns = useMemo(
+    () => [
+      {
+        accessorKey: 'name',
+        header: 'Name',
+        muiTableHeadCellProps: {align: 'center'},
+        muiTableBodyCellProps: {align: 'center'}
+      },      
+      {
+        accessorKey: 'country_details.name',
+        header: 'Country'
+      },      
+      {
+        accessorKey: 'league_details.name',
+        header: 'League'
+      },
+      {
+        accessorKey: 'city',
+        header: 'City'
+      },
+      {
+        accessorKey: 'attendance',
+        header: 'Attendance'
+      },
+      {
+        accessorKey: 'characteristic_names',
+        header: 'Characteristic',
+        Cell: ({cell}) => (
+          <div style={{display:'flex', gap:'8px', flexWrap: 'wrap'}}>
+            {
+              cell.getValue()?.map((char,index) => (
+                <Chip key={index} label={char}/>
+              ))
+            }
 
-  console.log(typeof tableData); // This should now log "object" (an array is a type of object in JS)
-  console.log(tableData)
+          </div>
+        )
+      }
+    ]
+  )
+
+  // const deleteTeam = (id) => {
+  //   setTableData(prevData => prevData.filter(team => team.id !== id))
+  //   AxiosInstance.delete(`club/${id}/`)
+  //     .then(() => console.log('Team Deleted'))
+  //     .catch(error => console.error('Error deleting team: ', error))
+  // }
 
   return (
-    <Box sx={{ marginLeft: '200px', marginRight: '10px'}}>
-      <h1>All Clubs</h1>
-      <AllTeamsTable tableData={tableData} onDelete={deleteTeam}/>
-    </Box>
+    <div>
+    <Box className={'TopBar'} sx={{marginLeft: '100px'}}>
+        <ViewCompactIcon />
+        <Typography sx={{ marginLeft: '15px', fontWeight: 'bold' }} variant='subtitle2'>
+          All Teams
+        </Typography>      
+      </Box>
+      <MaterialReactTable
+        columns={columns}
+        data={myData}
+      />
+
+    </div>
   );
 };
 
